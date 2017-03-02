0093704ee318336da6a7c74da6f478c5c97a0664
MariaRafailova <mrafailova@gmail.com>
2017-03-02 10:47:05 +0200

    Add files via upload

---
+++ CountRealNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumbers
+{
+    public class CountRealNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            var result = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!result.ContainsKey(number))
+                {
+                    result[number] = 0;
+                }
+                result[number]++;
+            }
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
